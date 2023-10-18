95900eb0b07ccc977c7da874c2f4402c2eb84ddb
saudNA9 <c3026696@newcastle.ac.uk>
2023-10-18 17:51:55 +0300

    In the cli.py file i have added the scale argument.

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -7,6 +7,8 @@ def cli():
     parser = argparse.ArgumentParser(prog="walking_panda")
     parser.add_argument("--no-rotate", help="Suppress Rotation",
                         action="store_true")
+    parser.add_argument("--scale", type=int, help="Scale Panda Size",
+                        )
     args = parser.parse_args()
 
     walking = panda.WalkingPanda(**vars(args))
