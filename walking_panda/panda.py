77044aaba663729bbab9a23630e098b16beae5fe
saudNA9 <c3026696@newcastle.ac.uk>
2023-10-18 16:55:24 +0300

    here i wrote the code to run the panda

--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -24,7 +24,7 @@ class WalkingPanda(ShowBase):
             self.taskMgr.add(self.stopCameraTask, "StopCameraTask")
 
         # Load and transform the panda actor.
-        self.pandaActor = Actor("models/panda-model",
+        self.pandaAct3or = Actor("models/panda-model",
                                 {"walk": "models/panda-walk4"})
         self.pandaActor.setScale(0.005, 0.005, 0.005)
         self.pandaActor.reparentTo(self.render)
