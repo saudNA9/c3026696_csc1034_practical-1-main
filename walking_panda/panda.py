52bb889b976d08409f6f781edd5865bbfcabc98b
saudNA9 <c3026696@newcastle.ac.uk>
2023-10-18 17:36:04 +0300

    here I have added the scale argument to panda.py.

--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -16,12 +16,23 @@ class WalkingPanda(ShowBase):
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
 
+        if scale != None and scale != 0:
+            if scale == 1:
+                scale = 0.008
+        else:
+            scale = 0.004
+
         if no_rotate==False:
             # Add the spinCameraTask procedure to the task manager.
             self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-
         else:
             self.taskMgr.add(self.stopCameraTask, "StopCameraTask")
+
+        # transform and load the actor of panda.
+        self.pandaActor = Actor("models/panda-model",
+                                    {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(scale, scale, scale)
+        self.pandaActor.reparentTo(self.render)
 
         # Load and transform the panda actor.
         self.pandaAct3or = Actor("models/panda-model",
