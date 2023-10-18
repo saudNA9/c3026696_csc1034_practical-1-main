13cc9403396dd1341342f4f355301258c911b83f
saudNA9 <c3026696@newcastle.ac.uk>
2023-10-19 02:43:28 +0300

    Finally i did a quick review, and i tried to change some values to figure the diffrences between them.

--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -1,79 +1,76 @@
 from math import pi, sin, cos
-
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
+
 
 class WalkingPanda(ShowBase):
     def __init__(self, scale, scale_nature, nature_position, panda_jogging, no_rotate=False):
         ShowBase.__init__(self)
 
-        # Load the environment model.
-        self.scene = self.loader.loadModel("models/environment")
-        # Reparent the model to render.
-        self.scene.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-8, 42, 0)
+        # Here i verified command-line arguments
+        if no_rotate == None:
+            no_rotate = False
 
         if scale != None and scale != 0:
             if scale == 1:
-                scale = 0.008
+                scale = 0.018
         else:
-            scale = 0.004
+            scale = 0.009
 
         if scale_nature == None:
-            scale_nature = 0.20
+            scale_nature = 0.30
 
         if nature_position != None:
             if nature_position == "left":
-                nature_position = [8, 40, 0]
+                nature_position = [6, 40, 0]
 
             if nature_position == "right":
-                nature_position = [-16, 40, 0]
+                nature_position = [-12, 40, 0]
         else:
-            nature_position = [-6, 40, 0]
+            nature_position = [-4, 40, 0]
 
         if panda_jogging == None:
-            panda_jogging = 1
+            panda_jogging = 5
 
-        # Load the nature model.
-        self.scene = self.loader.loadModel("models/nature")
-        # Reparent the model to render.
+        # Here i activated the nature model.
+        self.scene = self.loader.loadModel("models/environment")
+        # Here we reparent the model in order for it to render.
         self.scene.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-         self.scene.setScale(scale_nature, scale_nature, scale_nature)
-         self.scene.setPos(nature_position[0], nature_position[1], nature_position[2])
+        # On the model,  we use scale and position transformations.
+        self.scene.setScale(scale_nature, scale_nature, scale_nature)
+        self.scene.setPos(nature_position[0], nature_position[1], nature_position[2])
 
-        if no_rotate==False:
-            # Add the spinCameraTask procedure to the task manager.
+        if no_rotate == False:
+            # Here we add the procedure spinCameraTask to the task manager.
             self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
         else:
+            # Here stop spinCameraTask procedure from the task manager.
             self.taskMgr.add(self.stopCameraTask, "StopCameraTask")
 
-        # transform and load the actor of panda.
+        # Here we load the panda actor and transform it.
         self.pandaActor = Actor("models/panda-model",
-                                    {"walk": "models/panda-walk4"})
+                                {"walk": "models/panda-walk4"})
         self.pandaActor.setScale(scale, scale, scale)
         self.pandaActor.reparentTo(self.render)
 
-        # Loop its animation.
+        # Here the animation should be looped.
         self.pandaActor.loop("walk")
 
-       # Change the play rate
-       self.pandaActor.setPlayRate(panda_jogging, "walk")
+        # Here we adjust the playback speed
+        self.pandaActor.setPlayRate(panda_jogging, "walk")
 
-        # Play Forest sound
-        forestSound = self.loader.loadSfx("Forest_sound.wav")
-        forestSound.setLoop(True)
-        forestSound.play()
+        # Here i generated a forest sound.
+        ForestSound = self.loader.loadSfx("Forest_Sound.wav")
+        ForestSound.setLoop(True)
+        ForestSound.play()
 
-        # Play panda sound
+        # Here i generated panda sound
         pandaSound = self.loader.loadSfx("Panda_Sound.wav")
         pandaSound.setLoop(True)
         pandaSound.play()
 
-    # Define a procedure to move the camera.
+    # here i created a technique for moving the camera.
     def spinCameraTask(self, task):
         angleDegrees = task.time * 6.0
         angleRadians = angleDegrees * (pi / 180.0)
