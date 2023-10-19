b6915d1765d4ab7d34c564874eb27d9dcbac406c
saudNA9 <c3026696@newcastle.ac.uk>
2023-10-19 20:14:04 +0300

    Here i have adjusted some comments.

--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -12,15 +12,18 @@ class WalkingPanda(ShowBase):
         if no_rotate == None:
             no_rotate = False
 
+        # Here i have wrote the scale argument
         if scale != None and scale != 0:
             if scale == 1:
                 scale = 0.018
         else:
             scale = 0.009
 
+        #Here i have wrote the scale_nature argument
         if scale_nature == None:
             scale_nature = 0.30
 
+        #Here i have wrote the nature_position argument
         if nature_position != None:
             if nature_position == "left":
                 nature_position = [6, 40, 0]
@@ -29,7 +32,7 @@ class WalkingPanda(ShowBase):
                 nature_position = [-12, 40, 0]
         else:
             nature_position = [-4, 40, 0]
-
+        #Here i have added the panda_jogging argument
         if panda_jogging == None:
             panda_jogging = 5
 
@@ -42,10 +45,10 @@ class WalkingPanda(ShowBase):
         self.scene.setPos(nature_position[0], nature_position[1], nature_position[2])
 
         if no_rotate == False:
-            # Here we add the procedure spinCameraTask to the task manager.
+        # Here we add the procedure spinCameraTask to the task manager.
             self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
         else:
-            # Here stop spinCameraTask procedure from the task manager.
+        # Here stop spinCameraTask procedure from the task manager.
             self.taskMgr.add(self.stopCameraTask, "StopCameraTask")
 
         # Here we load the panda actor and transform it.
@@ -70,14 +73,14 @@ class WalkingPanda(ShowBase):
         pandaSound.setLoop(True)
         pandaSound.play()
 
-    # here i created a technique for moving the camera.
+    # Here i created a technique for moving the camera.
     def spinCameraTask(self, task):
         angleDegrees = task.time * 6.0
         angleRadians = angleDegrees * (pi / 180.0)
         self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
         self.camera.setHpr(angleDegrees, 0, 0)
         return Task.cont
-
+    # Here i created another technique were it stops the camera
     def stopCameraTask(self, task):
         angleDegrees = 0
         angleRadians = angleDegrees * (pi / 180.0)
